d001397a0b969505afee459becbde217773fc404
Nick <nick.keller57@gmail.com>
2016-06-08 22:30:29 -0400

    Added other basic stuff

---
+++ GamesHub/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GamesHub
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            //check some stuff in documents, make sure it's there.
+
+
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new SelectRootScreen());
+        }
+    }
+}
